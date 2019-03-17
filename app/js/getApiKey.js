b6e07be2318c36606b8b65ef8aa4aa8e6cd6b7e2
Javed Akhtar <akhtar.ja@gmail.com>
2019-03-17 15:50:37 -0700

    Resolves #34 - added functionality to issue an API key on successful login

---
+++ app/js/getApiKey.js
@@ -0,0 +1,20 @@
+(function($) {
+  'use strict';
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+  }
+
+  const apiKey = getQueryVariable('apiKey');
+
+  $('#api-key').html(apiKey);
+  $('#api-url').html(`http://spotify-now-playing.javedakhtar.io?apiKey=${apiKey}`);
+})(jQuery);
